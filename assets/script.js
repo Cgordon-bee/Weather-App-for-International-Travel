1bd68ca01da75d7c818d2d3e7f332412902a649e
Cgordon-bee <81937022+Cgordon-bee@users.noreply.github.com>
2021-07-11 19:00:23 +0100

    update to js

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,36 @@
+var API_KEY = "1d7f4a93892a901d16a2035551c08c50";
+var generateEndPoint = city;
+var citySelectionForm = "citySelection";
+var citySelectionInput = "input";
+var weatherCard = 
 
+function generateUrl (city) {
+
+
+
+
+
+}
+function generateEndPoint (city) {
+    var NUMBER_OF_DAYS_TO_REQUEST = 5; 
+    return https/api.openweathermap
+
+
+
+}
+
+function makeRequest (EndPoint) {
+
+
+
+}
+
+function creatWeatherCard (dayweatherInfo) {
+
+var card = $("<div>");
+var temp = $("<p>");
+var humidity =$ ("<p>");
+
+    
+
+}
