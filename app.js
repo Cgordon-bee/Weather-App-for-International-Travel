b4dfe076704a7482922aeb7a160c18cee1740a6d
Cgordon-bee <81937022+Cgordon-bee@users.noreply.github.com>
2021-10-21 17:12:36 +0100

    updated UI

---
+++ app.js
@@ -0,0 +1,5 @@
+$.getJSON("https://api.openweathermap.org/data/2.5/weather?q=london&units=imperial&appid=a24573286986d0f489dc46b86371d57c", function(data){
+console.log(data)
+
+
+})
--- assets/script.js
+++ assets/script.js
@@ -1,48 +1,85 @@
-var API_KEY = "1d7f4a93892a901d16a2035551c08c50";
-var generateEndPoint = "city";
-var citySelectionForm = "citySelection";
-var citySelectionInput = "input";
-var weatherCard = "weatherForecast";
+
+var API_KEY = "17cd08020468c424106a1913f27217e9";
+var citySelectionForm=$("#citySelectionForm");
+var button = document.getElementById("btn");
 var citySelectionInput = $("#citySelectionInput");
+var city 
+
+
+//Function for user's request from input form
+
+function GetInfo(city) {
+
+     return fetch (`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`)
+    .then(function (res) {
+        return res.json();
+    
+
+    
+}).catch(function (err){
+    console.log(err)
+})
+
+};
+
+//This is the function for updating the UI once info received JQuery's shorthand to upload info to UI page
+function renderCurrentWeather (dayweatherInfo) {
+
+ 
+
+    //var card = $("<div>");
+    //var temp = $("<p id='temp'></p>");
+    var wind = $("<p id='wind'></p>")
+    var humidity =$("<p id='humidity'></p>");
 
 
 
-function generateUrl (city) {
+    $("#temp").text(dayweatherInfo.main.temp)
+    $("#wind").text(dayweatherInfo.wind.speed)
+    $("#humidity").text(dayweatherInfo.main.humidity)
 
-
-
-
+    // $("#dayweatherInfo").append(temp)  
 }
-function generateEndPoint (city) {
-    var NUMBER_OF_DAYS_TO_REQUEST = 5; 
 
-    return `https://api.openweathermap.org/data/2.5/forecast/daily?q=${city}&cnt=${NUMBER_OF_DAYS_TO_REQUEST}&appid=${API_KEY}`;
+//This is the function to obtain the weather forecast for the 5 days - a second request to the API to obtain the Latitude and Longitude, this will then be rendered to the UI Page
+   function getForeCastInfo(lat,lon) {
+   return fetch (`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${API_KEY}`)
+   .then(function (res) {
+       return res.json();
+   
+
+   
+}).catch(function (err){
+   console.log(err)
+})
 
 
-
+// This is the scripts that will extract the data from the json, we use the loop method to sort and then append information to the UI page
 }
 
-function makeRequest (endPoint) {
-    return fetch (EndPoint).then(function (res) {
-    return res.json();
 
+
+
+//onclick- this is the function that will listen for the users details in the "search city box"
+
+$("#btn").on("click", async function (event) {
+    event.preventDefault();
+
+// jquery= j.query
+city =citySelectionForm.val()
+var data = await GetInfo(city)
+console.log(data)
+renderCurrentWeather(data)
+var forcasteData = await getForeCastInfo(data.coord.lat, data.coord.lon)
+console.log(forcasteData)
 
 });
 
-}
 
 
-function creatWeatherCard (dayweatherInfo) {
 
-var card = $("<div>");
-var temp = $("<p>");
-var humidity =$ ("<p>");
 
-citySelectionForm.on ("submit"); function (event) {
-event.preventdefault();
+//this is the element for updating the the UI, we will use JQuery shorthand to manipulate the data
 
-var selectedcity = citySelectionInput.valueOf();
-}
 
-}
 
