9b49d73be6260176617101d96b7dea2b3c27c2e7
Cgordon-bee <81937022+Cgordon-bee@users.noreply.github.com>
2021-10-24 22:31:41 +0100

    fixed for index.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-$.getJSON("https://api.openweathermap.org/data/2.5/weather?q=london&units=imperial&appid=a24573286986d0f489dc46b86371d57c", function(data){
-console.log(data)
+
+// $.getJSON("https://api.openweathermap.org/data/2.5/weather?q=london&units=imperial&appid=a24573286986d0f489dc46b86371d57c", function(data){
+// console.log(data)
 
 
-})
+// })
